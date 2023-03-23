a2bc23c557365775f12f485544cf78a8c12e090e
ikaera <ikaera@gmail.com>
2023-03-22 23:09:59 -0400

    change loggedIn with logged_in to be consistant.

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,6 @@
-// const session = require('express-session');
-
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
-  if (!req.session.loggedIn) {
+  if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
     next();
