30e50d9acb4cdd313e5fb6d598c2e257a55195dc
Nate J <nathan.evan.johnson@gmail.com>
2023-03-23 05:18:54 -0700

    initial stab at developing fields for sql and sequelized schema

---
+++ models/Listing.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Listing extends Model {}
+
+Listing.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    format: {
+      type: DataTypes.ENUM('CD', 'vinyl', 'cassette'),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    release_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    // cover_art: {
+    //   // type: DataTypes.FLOAT,
+    //   allowNull: true,
+    // },
+    condition: {
+      type: DataTypes.ENUM('New', 'Good', 'Fair'),
+      allowNull: false,
+    },
+    label: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'listing',
+  }
+);
+
+module.exports = Listing;
