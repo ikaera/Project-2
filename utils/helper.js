a99a06c07de56f4844f843f8a533c93494f20322
ikaera <ikaera@gmail.com>
2023-03-22 22:39:49 -0400

    add helper.js.

--- utils/helper.js
+++ utils/helper.js
@@ -4,5 +4,12 @@ module.exports = {
   getEmoji: () => {
     const randomNumber = Math.random();
     // Return a random emoji
+    if (randomNumber > 0.6) {
+      return `<span for="img" aria-label="lightbulb">📀 🎧</span>`;
+    } else if (randomNumber > 0.3) {
+      return `<span for="img" aria-label="laptop">💽 🎧</span>`;
+    } else {
+      return `<span for="img" aria-label="gear"> 💿 🎵 </span>`;
+    }
   },
 };
