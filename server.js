f00991d04a716bafb4bfb44f1a95167466ad1640
ikaera <ikaera@gmail.com>
2023-03-26 23:23:03 -0400

    add Server listening on: http://localhost: + PORT.

--- server.js
+++ server.js
@@ -78,5 +78,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('NOW LISTENING'));
+  app.listen(PORT, () =>
+    console.log('Server listening on: http://localhost:' + PORT)
+  );
 });
