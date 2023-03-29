41581c96e96908fcea0b8c31ab3bb3dfff4a4c66
ikaera <ikaera@gmail.com>
2023-03-29 19:34:59 -0400

    add router.delete to the favoriteRoutes.js.

--- controllers/api/favoritesRoutes.js
+++ controllers/api/favoritesRoutes.js
@@ -4,14 +4,32 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await Favorites.create({
+    const favoritesData = await Favorites.create({
       favitem_id: req.body.id,
       user_id: req.session.user_id,
     });
-    console.log(userData);
-    res.status(200).json(userData);
+    console.log(favoritesData);
+    res.status(200).json(favoritesData);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const favoritesData = await Favorites.destroy({
+      where: {
+        id: req.params.id,
+        user_ic: req.session.user_id,
+      },
+    });
+
+    if (!favoritesData) {
+      res.status(400).json({ message: 'No saved itme found with this ID!' });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
