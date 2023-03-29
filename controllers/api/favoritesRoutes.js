4e5c7592807c99c860c0e83897277bbfc2d7b8e9
ikaera <ikaera@gmail.com>
2023-03-29 17:17:59 -0400

    upadte controllers/api/index.js file.

---
+++ controllers/api/favoritesRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const { favorites } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const listingRoutes = require('./listingRoutes');
+const favoritesRoutes = require('./favoritesRoutes');
+const favItemRoutes = require('./favItemRoutes');
 
 router.use('/users', userRoutes);
 router.use('/listings', listingRoutes);
+router.use('favorites', favoritesRoutes);
+router.use('/favItem', favItemRoutes);
 
 module.exports = router;
