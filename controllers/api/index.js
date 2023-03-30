0e7ce23c5a7ef3153546a023cd54e30ff6ab9dcc
Nate J <nathan.evan.johnson@gmail.com>
2023-03-29 22:55:53 -0700

    fixed routes for cart and cart item

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const listingRoutes = require('./listingRoutes');
 
-/* const cartRoutes = require('./cartRoutes');
-const cartItemRoutes = require('/.cartItemRoutes'); */
+const cartRoutes = require('./cartRoutes');
+const cartItemRoutes = require('./cartItemRoutes');
 
 const favoritesRoutes = require('./favoritesRoutes');
 const favItemRoutes = require('./favItemRoutes');
 
-/* router.use('/cart', cartRoutes);
-router.use('/cartItem', cartItemRoutes); */
+router.use('/cart', cartRoutes);
+router.use('/cartItem', cartItemRoutes);
 router.use('/favorites', favoritesRoutes);
 router.use('/favItem', favItemRoutes);
 router.use('/users', userRoutes);
